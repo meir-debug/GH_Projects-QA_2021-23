7c5ecf2968dd2468247f27013e9c7b9d80dbc6e9
Your Name <meir.bar.shay@gmail.com>
2023-09-27 18:12:44 +0300

    Update code

--- AliceBlueFlorist/AliceBF_LogIn.py
+++ AliceBlueFlorist/AliceBF_LogIn.py
@@ -8,7 +8,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import Helpers as Hp
 import unittest
-import AllureReports
+
+
+# import AllureReports
 
 
 # import HtmlTestRunner
@@ -24,7 +26,7 @@ class LogIn(unittest.TestCase):
         driver = self.driver
         # workflow over "elem" and "wait" variable to better code length
         elem = driver.find_element
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 5)
         driver.get(Hp.url_Test)
 
         # API testing from Selenium
@@ -32,10 +34,7 @@ class LogIn(unittest.TestCase):
               "as status Code")
         code = requests.get(Hp.url_Test).status_code
 
-        if code == 200:
-            print("API response code is OK")
-        else:
-            print("API response code is not 200")
+        print("API response code is OK") if code == 200 else print("API response code is not 200")
 
         if Hp.Title not in driver.title:
             raise Exception("AliceBlue Florist - page has wrong Title!")
@@ -52,7 +51,7 @@ class LogIn(unittest.TestCase):
         # Checking page title
         self.assertIn(Hp.Title, driver.title)
         print("Page has", driver.title + " as Page title")
-        elem(By.XPATH, "//*[@class='_1UDJF']").click()
+        # elem(By.XPATH, "//*[@class='_1UDJF']").click()
         Hp.delay()
         try:
             wait.until(
@@ -63,13 +62,15 @@ class LogIn(unittest.TestCase):
             driver.get_screenshot_as_file(Hp.error_png)
             driver.save_screenshot(Hp.error_png)
         Hp.delay()
-        elem(By.XPATH, "//span[@class='_1Qjd7'][contains(.,'Sign up with email')]").click()
+        elem(By.XPATH, Hp.logIn).click()
         Hp.delay()
+        elem(By.XPATH, Hp.sUp_withEmail).click()
         elem(By.XPATH, Hp.input_em).clear()
-        elem(By.XPATH, Hp.input_em).send_keys("meir.bar.shay+6@gmail.com")  # Hp.fake.email()
+        elem(By.XPATH, Hp.input_em).send_keys(Hp.logEmail)  # Hp.fake.email()
+        Hp.delay()
         elem(By.XPATH, Hp.input_pas).clear()
-        elem(By.XPATH, Hp.input_pas).send_keys("1987365TY" + Keys.ENTER)  # Hp.fake.ean(length=8)
-        time.sleep(10)
+        elem(By.XPATH, Hp.input_pas).send_keys(Hp.logPass)  # Hp.fake.ean(length=8)
+        Hp.delay()
         try:
             wait.until(EC.visibility_of_element_located((By.XPATH, Hp.input_em)))
             print("Loading took too much time!")
@@ -81,12 +82,11 @@ class LogIn(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-
 # if __name__ == '__main__':
 #     unittest.main(
 #         testRunner=HtmlTestRunner.HTMLTestRunner(output='./HtmlReports'))
 
-if __name__ == '__main__':
-    unittest.main(AllureReports)
+# if __name__ == '__main__':
+#   unittest.main(AllureReports)
 
 # py.test --alluredir=./AllureReports ./unittest4.py
